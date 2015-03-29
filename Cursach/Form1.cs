6133fff00ff315f7541481207164052be8b3dc1e
Anna <anna@sakhnenko.ru>
2015-03-29 21:50:49 +0300

    1

--- Cursach/Form1.cs
+++ Cursach/Form1.cs
@@ -20,10 +20,17 @@ namespace Cursach
         private void button1_Click(object sender, EventArgs e)
         {
             Parser p = new Parser();
-            string container = p.getRequest("http://www.riviera-sochi.ru/tours/275");
-            p.Check(container);
-            textBox1.Text = p.HOTEL + " " + p.DATE + " " + p.NAME;
+            string container = p.getRequest("http://www.riviera-sochi.ru/tours/");
+            string Block = p.Block(container);
+            while (p.STOP == false)
+            {
+                Block = p.Block1(Block);
+                p.Check1(p.BLOCK_WORK);
+                string URL = "http://www.riviera-sochi.ru/tours/" + p.ID;
+                container = p.getRequest(URL);
+                p.Check(container);
+                textBox1.Text += p.ID + " " + p.DURATION + " " + p.PRICE + " " + p.RESORT + " " + p.DATE + " " + p.HOTEL + " " + p.NAME;
+            }
         }
-
     }
 }
